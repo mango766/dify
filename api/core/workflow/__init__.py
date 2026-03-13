42f930d00e3e97237ae1bb353b572378c6391aa6
WH-2099 <wh2099@pm.me>
2026-03-13 14:17:57 +0800

    draft

--- api/core/workflow/__init__.py
+++ api/core/workflow/__init__.py
@@ -1,4 +1,7 @@
 from .node_factory import DifyNodeFactory
+from .node_resolution import ensure_workflow_nodes_registered
 from .workflow_entry import WorkflowEntry
+
+__all__ = ["DifyNodeFactory", "WorkflowEntry", "ensure_workflow_nodes_registered"]
 
 __all__ = ["DifyNodeFactory", "WorkflowEntry"]
